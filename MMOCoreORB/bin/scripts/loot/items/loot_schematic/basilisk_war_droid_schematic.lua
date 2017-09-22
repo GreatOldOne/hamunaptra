05865f777e3b1554f35ef18e5ce6d4b2039267b0
merik444 <whitelight_illumina@hotmail.com>
2017-09-22 09:06:36 -0400

    [Test] Craftable War Droid
    Testing adding a loot schematic to make the war droid craftable

---
+++ MMOCoreORB/bin/scripts/loot/items/loot_schematic/basilisk_war_droid_schematic.lua
@@ -0,0 +1,12 @@
+basilisk_war_droid_schematic = {
+	minimumLevel = 0,
+	maximumLevel = -1,
+	customObjectName = "",
+	directObjectTemplate = "object/tangible/loot/loot_schematic/basilisk_war_droid_schem.iff",
+	craftingValues = {
+	},
+	customizationStringNames = {},
+	customizationValues = {}
+}
+
+addLootItemTemplate("basilisk_war_droid_schematic", basilisk_war_droid_schematic)
